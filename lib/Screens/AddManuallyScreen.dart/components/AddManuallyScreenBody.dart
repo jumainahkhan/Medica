195b07bc25abf582356e670a7691e24708c82581
Saif <hishamkolikara@gmail.com>
2022-12-09 20:00:39 +0530

    Prescription Screen

--- lib/Screens/AddManuallyScreen.dart/components/AddManuallyScreenBody.dart
+++ lib/Screens/AddManuallyScreen.dart/components/AddManuallyScreenBody.dart
@@ -1,8 +1,10 @@
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'package:get/get.dart';
 import 'package:google_fonts/google_fonts.dart';
 import 'package:heroicons/heroicons.dart';
 import 'package:hexcolor/hexcolor.dart';
+import 'package:medica/Controller/MedLogController.dart';
 import 'package:medica/Screens/AddManuallyScreen.dart/components/buildMedDetail.dart';
 import 'package:medica/Screens/AddManuallyScreen.dart/components/buildVisitDetail.dart';
 import 'package:responsive_sizer/responsive_sizer.dart';
@@ -15,41 +17,39 @@ class AddManuallyScreenBody extends StatefulWidget {
 }
 
 class _AddManuallyScreenBodyState extends State<AddManuallyScreenBody> {
+  MedLogController medLogController = Get.put(MedLogController());
   @override
   Widget build(BuildContext context) {
-    return Stack(
+    return Column(
+      mainAxisAlignment: MainAxisAlignment.start,
+      crossAxisAlignment: CrossAxisAlignment.start,
       children: [
-        Positioned(
-          bottom: 35,
-          right: 35,
-          child: FloatingActionButton.extended(
-            backgroundColor: HexColor('#6d69f0'),
-            onPressed: () {
-              // TODO: Submit form action
-            },
-            label: Row(
-              children: const [
-                Icon(Icons.save),
+        appBar(context),
+        Expanded(
+          child: SingleChildScrollView(
+            child: buildForm(),
+          ),
+        ),
+        Container(
+            color: HexColor('#6d69f0'),
+            height: 55,
+            width: double.infinity,
+            child: Row(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                Icon(
+                  Icons.save,
+                  color: Colors.white,
+                ),
                 SizedBox(
                   width: 10,
                 ),
-                Text("Save"),
+                Text(
+                  "Save",
+                  style: GoogleFonts.poppins(fontSize: 17, color: Colors.white),
+                ),
               ],
-            ),
-          ),
-        ),
-        Column(
-          mainAxisAlignment: MainAxisAlignment.start,
-          crossAxisAlignment: CrossAxisAlignment.start,
-          children: [
-            appBar(context),
-            Expanded(
-              child: SingleChildScrollView(
-                child: buildForm(),
-              ),
-            )
-          ],
-        ),
+            ))
       ],
     );
   }
@@ -87,7 +87,7 @@ class _AddManuallyScreenBodyState extends State<AddManuallyScreenBody> {
 
   buildForm() {
     return Padding(
-      padding: const EdgeInsets.fromLTRB(20, 0, 20, 0),
+      padding: const EdgeInsets.fromLTRB(20, 0, 20, 20),
       child: Column(
         crossAxisAlignment: CrossAxisAlignment.start,
         children: [
@@ -112,7 +112,7 @@ class _AddManuallyScreenBodyState extends State<AddManuallyScreenBody> {
             height: 30,
           ),
           buildMedDetail(),
-          buildMedDetail()
+          //buildMedDetail()
         ],
       ),
     );
--- lib/Screens/AddManuallyScreen.dart/components/buildMedDetail.dart
+++ lib/Screens/AddManuallyScreen.dart/components/buildMedDetail.dart
@@ -1,127 +1,135 @@
 import 'package:flutter/material.dart';
+import 'package:get/get.dart';
 import 'package:google_fonts/google_fonts.dart';
 import 'package:hexcolor/hexcolor.dart';
+import 'package:medica/Controller/MedLogController.dart';
 
 Column buildMedDetail() {
+  MedLogController medLogController = Get.put(MedLogController());
   return Column(
     children: [
       Row(
-        mainAxisAlignment: MainAxisAlignment.start,
+        mainAxisAlignment: MainAxisAlignment.spaceBetween,
         children: [
           Text(
-            "Visit Detail",
+            "Medicine Details",
             style: GoogleFonts.lato(
               fontSize: 18,
               color: HexColor('#6d69f0'),
               fontWeight: FontWeight.w600,
             ),
           ),
+          Row(
+            children: const [
+              Text("M"),
+              SizedBox(
+                width: 40,
+              ),
+              Text("A"),
+              SizedBox(
+                width: 40,
+              ),
+              Text("N"),
+              SizedBox(
+                width: 30,
+              ),
+            ],
+          )
         ],
       ),
       const SizedBox(
         height: 10,
       ),
-      Container(
-        width: double.infinity,
-        decoration: BoxDecoration(border: Border.all(color: Colors.black38)),
-        child: Padding(
-          padding: const EdgeInsets.fromLTRB(12, 8, 12, 8),
-          child: Column(
-            mainAxisAlignment: MainAxisAlignment.start,
-            children: [
-              Row(
-                crossAxisAlignment: CrossAxisAlignment.start,
-                children: [
-                  Expanded(
-                      child: TextField(
-                    decoration: InputDecoration(
-                      hintText: 'Enter the date of your visit',
-                      hintStyle: GoogleFonts.lato(
-                          fontSize: 18, fontWeight: FontWeight.w500),
-                      border: InputBorder.none,
-                      icon: Icon(
-                        Icons.calendar_month,
-                        color: HexColor('#6d69f0'),
+      GetBuilder<MedLogController>(builder: (context) {
+        return ListView.builder(
+            shrinkWrap: true,
+            itemCount: medLogController.medCount,
+            itemBuilder: (context, index) {
+              return Container(
+                width: double.infinity,
+                child: Padding(
+                  padding: const EdgeInsets.fromLTRB(12, 8, 12, 8),
+                  child: Column(
+                    mainAxisAlignment: MainAxisAlignment.start,
+                    children: [
+                      Row(
+                        crossAxisAlignment: CrossAxisAlignment.start,
+                        children: [
+                          Expanded(
+                              child: TextField(
+                            decoration: InputDecoration(
+                              hintText: 'Medicine Name',
+                              hintStyle: GoogleFonts.lato(
+                                  fontSize: 18, fontWeight: FontWeight.w500),
+                              border: InputBorder.none,
+                              icon: Icon(
+                                Icons.medication_liquid_outlined,
+                                color: HexColor('#6d69f0'),
+                              ),
+                            ),
+                          )),
+                          Checkbox(
+                            value: false,
+                            onChanged: (value) {},
+                          ),
+                          Checkbox(
+                            value: false,
+                            onChanged: (value) {},
+                          ),
+                          Checkbox(
+                            value: false,
+                            onChanged: (value) {},
+                          ),
+                        ],
                       ),
-                    ),
-                  ))
-                ],
-              ),
-              const SizedBox(
-                height: 10,
-              ),
-              const Divider(
-                color: Colors.black38,
-                height: 2,
-                thickness: 1,
-              ),
-              const SizedBox(
-                height: 10,
-              ),
-              Row(
-                crossAxisAlignment: CrossAxisAlignment.start,
-                children: [
-                  Expanded(
-                      child: TextField(
-                          decoration: InputDecoration(
-                    hintText: 'Name of the doctor',
-                    hintStyle: GoogleFonts.lato(
-                        fontSize: 18, fontWeight: FontWeight.w500),
-                    border: InputBorder.none,
-                    icon: Icon(Icons.person, color: HexColor('#6d69f0')),
-                  )))
-                ],
-              ),
-              const Divider(
-                color: Colors.black38,
-                height: 2,
-                thickness: 1,
-              ),
-              const SizedBox(
-                height: 10,
-              ),
-              Row(
-                crossAxisAlignment: CrossAxisAlignment.start,
-                children: [
-                  Expanded(
-                      child: TextField(
-                          decoration: InputDecoration(
-                    hintText: 'Name of the clinic',
-                    hintStyle: GoogleFonts.lato(
-                        fontSize: 18, fontWeight: FontWeight.w500),
-                    border: InputBorder.none,
-                    icon: Icon(Icons.local_hospital_outlined,
-                        color: HexColor('#6d69f0')),
-                  )))
-                ],
-              ),
-              const Divider(
-                color: Colors.black38,
-                height: 2,
-                thickness: 1,
-              ),
-              const SizedBox(
-                height: 10,
-              ),
-              Row(
-                crossAxisAlignment: CrossAxisAlignment.start,
-                children: [
-                  Expanded(
-                      child: TextField(
-                          decoration: InputDecoration(
-                    hintText: 'Reason of Visit',
-                    hintStyle: GoogleFonts.lato(
-                        fontSize: 18, fontWeight: FontWeight.w500),
-                    border: InputBorder.none,
-                    icon:
-                        Icon(Icons.question_answer, color: HexColor('#6d69f0')),
-                  )))
-                ],
-              ),
-            ],
+                      const SizedBox(
+                        height: 10,
+                      ),
+                      // const Divider(
+                      //   color: Colors.black38,
+                      //   height: 2,
+                      //   thickness: 1,
+                      // ),
+                      // const SizedBox(
+                      //   height: 10,
+                      // ),
+                    ],
+                  ),
+                ),
+              );
+            });
+      }),
+      Row(
+        mainAxisAlignment: MainAxisAlignment.end,
+        children: [
+          GestureDetector(
+              onTap: () {
+                medLogController.incrementMedCount();
+              },
+              child: Icon(Icons.add)),
+          SizedBox(
+            width: 10,
           ),
-        ),
-      ),
+          GetBuilder<MedLogController>(builder: (context) {
+            return Text(
+              medLogController.medCount.toString(),
+              style: GoogleFonts.lato(
+                fontSize: 20,
+                color: HexColor('#6d69f0'),
+                fontWeight: FontWeight.w600,
+              ),
+            );
+          }),
+          SizedBox(
+            width: 10,
+          ),
+          GestureDetector(
+              onTap: () {
+                medLogController.decrementMedCount();
+              },
+              child: Icon(Icons.remove))
+        ],
+      )
     ],
   );
 }
